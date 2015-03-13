44b02a954c6e73198122fbd11dff2af6e45d3f64
SerialGamer59 <camillebreuzon@orange.fr>
2015-03-13 10:14:50 +0100

    index modif, show formulaire de creation de compte

--- html/js/base.js
+++ html/js/base.js
@@ -2,14 +2,30 @@ var FORM_LOGIN = "<form>" +
 								 	"<input type=\"text\" placeholder=\"login\" id=\"login\">" +
 									"<input type=\"password\" placeholder=\"password\"id=\"passwd\">" +
 									"<input type=\"submit\" value=\"envoyer\" id=\"submit\">" +
-								"</form>" + 
-								"Pas de compte ? <a href=\"./signIn.html\">Cree en un !</a><br>" +
+								"</form>";
+								
+var LINK_CREATE = "Pas de compte ? <button id=\"button_create\">Cree en un !</button><br>" +
 								"<a href=\"./lostPasswd.html\">Mot de passe perdu</a>";
 
 var MSG_WELCOME = "<a href=\"./index.html\">Elo World</a>";
 
+var FORM_CREATE = "<center><h3>Creer un compte :</h3>" +
+		"<form>" +
+"<input type=\"text\" placeholder=\"pseudo\" id=\"pseudo\">"+
+"<input type=\"password\" placeholder=\"password\" id =\"passwd\">"+
+"<input type=\"password\" placeholder=\"repeat password\" id=\"verif_passwd\">"+
+"<input type=\"text\" placeholder=\"e-mail\" id=\"mail\">"+
+"<input type=\"button\" id=\"submit\" value=\"envoyer\">"+
+"</form></center>";
+
 function login(){
 	$("#login").append(FORM_LOGIN);
+	$("#login").append(LINK_CREATE);	
+}
+
+function create(){
+	$("#create").append(FORM_CREATE);
+	$("#create").hide();
 }
 
 function verifSyn(str){
