6cf101a878ec9853d3eb7318676d0f3665e11788
Florent SICCHIO <f.sicchio@gmail.com>
2015-03-17 13:14:34 +0100

    patate

--- src/main/java/servlet/Loging.java
+++ src/main/java/servlet/Loging.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Path;
+import javax.ws.rs.client.ClientBuilder;
 import bdd.joueur.*;
 
 @Path("/loging")
@@ -13,16 +14,7 @@ public class Loging extends HttpServlet {
 
     public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException {
 			/*trouver comment utiliser le dao*/
-			/*Joueur j = ClientBuilder().newClient()
-																.target()
-																.request()
-																.get(Joueur.class);
-
-			if(j != null){
-				HttpSession session = request.getSession();
-				session.setAttribute("login",request.getParameter("login"));
-			}
-			response.sendRedirect("index.jsp");*/
+			response.sendRedirect("index.jsp");
     }
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException {
