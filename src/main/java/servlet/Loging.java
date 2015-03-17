c5af1c34d1e548f11248497bd35c5dbb2e2ad6d6
Florent SICCHIO <f.sicchio@gmail.com>
2015-03-17 08:55:48 +0100

    serv pret a rececoir les servlet

--- src/main/java/servlet/Loging.java
+++ src/main/java/servlet/Loging.java
@@ -1,23 +1,31 @@
+package servlet;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.Path;
 import bdd.joueur.*;
 
+@Path("/loging")
 public class Loging extends HttpServlet {
 
-    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException {
-			Joueur j = new JoueurDao().findJoueur(request.getParameter("login"), request.getParameter("passwd"));
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException {
+			/*trouver comment utiliser le dao*/
+			/*Joueur j = ClientBuilder().newClient()
+																.target()
+																.request()
+																.get(Joueur.class);
 
 			if(j != null){
 				HttpSession session = request.getSession();
 				session.setAttribute("login",request.getParameter("login"));
 			}
-			response.sendRedirect("index.jsp");
+			response.sendRedirect("index.jsp");*/
     }
 
-    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException {
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException {
 
     }
 
